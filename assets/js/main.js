fbef8e2e6232b79bf266cb1bf4defec78c4e9188
Nmaejone <devnamejone@gmail.com>
2021-07-07 16:53:46 +0600

    Mazadent design with out Footer.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+    "use strict";
+    
+    // Banner-sldier
+    $('.banner-sldie-active').owlCarousel({
+        loop:true,
+        margin:0,
+        items:1,
+        navText:['<i class="fal fa-angle-left"></i>','<i class="fal fa-angle-right"></i>'],
+        nav:true,
+        dots:true,
+    });
+
+
+    
+})(jQuery);
